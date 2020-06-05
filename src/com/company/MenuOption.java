7c037bd5d417881bc56b2483a44e6cd0863694e5
hit02 <bagnucki02@gmail.com>
2020-06-05 17:41:37 +0200

    implement MenuOption

--- src/com/company/MenuOption.java
+++ src/com/company/MenuOption.java
@@ -1,4 +1,27 @@
 package com.company;
 
 public class MenuOption {
+    public String description;
+    private boolean checked;
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void check() {
+        checked = true;
+    }
+
+    public void uncheck() {
+        checked = false;
+    }
+
+    public void flip() {
+        checked = !checked;
+    }
+
+    public String toString() {
+        char prompt = checked ? 'x' : ' ';
+        return String.format("%s [%c]", description, prompt);
+    }
 }
