3c0d175d3362f74e4fb286326e3219b7085e998e
hit02 <bagnucki02@gmail.com>
2020-06-07 12:04:41 +0200

    Implement fields of Project

--- src/com/company/Project.java
+++ src/com/company/Project.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class Project {
+    public enum ComplexityLevel {
+        EASY,
+        MEDIUM,
+        COMPLICATED
+    }
+    private String name;
+    private Dictionary<String, Integer> technologyWorkdays = new Hashtable<>();
+    private Human client;
+    private Date deadline;
+    private Price feeForMissingDeadline;
+    private Price price;
+    private int daysBeforePayment;
+    private ComplexityLevel complexity;
 }
