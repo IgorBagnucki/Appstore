e0adab4eb2e423575d6f731ca01ad5af91861eda
hit02 <bagnucki02@gmail.com>
2020-06-08 13:47:55 +0200

    rename Technologies to TechnologiesWrapper

--- src/com/company/Project.java
+++ src/com/company/Project.java
@@ -9,7 +9,7 @@ public class Project {
         COMPLICATED
     }
     private String name;
-    private Technologies<Integer> technologyWorkDays;
+    private TechnologiesWrapper<Integer> technologyWorkDays;
     private Human client;
     private Date deadline;
     private Price feeForMissingDeadline;
@@ -17,7 +17,18 @@ public class Project {
     private int daysToWaitBeforePayment;
     private ComplexityLevel complexity;
 
-    public Project(String name, Human client, Date deadline, Price feeForMissingDeadline, Price price, int daysToWaitBeforePayment) {
-
+    public Project(String name, Human client, Date deadline, Price feeForMissingDeadline, Price price, int daysToWaitBeforePayment, TechnologiesWrapper<Integer> technologyWorkDays) {
+        this.name = name;
+        this.client = client;
+        this.deadline = deadline;
+        this.feeForMissingDeadline = feeForMissingDeadline;
+        this.price = price;
+        this.daysToWaitBeforePayment = daysToWaitBeforePayment;
+        this.technologyWorkDays = technologyWorkDays;
+        int requiredTechnologies = technologyWorkDays.countNonZero();
+        complexity =
+                requiredTechnologies==1 ? ComplexityLevel.EASY :
+                requiredTechnologies==2 ? ComplexityLevel.MEDIUM :
+                                          ComplexityLevel.COMPLICATED;
     }
 }
---
+++ src/com/company/TechnologiesWrapper.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class TechnologiesWrapper<T> {
+    public T frontend;
+    public T backend;
+    public T database;
+    public T mobile;
+    public T wordpress;
+    public T prestashop;
+
+    TechnologiesWrapper(T defaultValue) {
+        frontend = defaultValue;
+        backend = defaultValue;
+        database = defaultValue;
+        mobile = defaultValue;
+        wordpress = defaultValue;
+        prestashop = defaultValue;
+    }
+
+    TechnologiesWrapper<T> frontend(T value) {
+        frontend = value;
+        return this;
+    }
+
+    TechnologiesWrapper<T> backend(T value) {
+        backend = value;
+        return this;
+    }
+
+    TechnologiesWrapper<T> database(T value) {
+        database = value;
+        return this;
+    }
+
+    TechnologiesWrapper<T> mobile(T value) {
+        mobile = value;
+        return this;
+    }
+
+    TechnologiesWrapper<T> wordpress(T value) {
+        wordpress = value;
+        return this;
+    }
+
+    TechnologiesWrapper<T> prestashop(T value) {
+        prestashop = value;
+        return this;
+    }
+
+    int addValues() {
+        return (int)frontend + (int)backend + (int)database + (int)mobile + (int)wordpress + (int)prestashop;
+    }
+
+    int countNonZero() {
+        return (int)frontend==0      ? 0 : 1
+                + (int)backend==0    ? 0 : 1
+                + (int)database==0   ? 0 : 1
+                + (int)mobile==0     ? 0 : 1
+                + (int)wordpress==0  ? 0 : 1
+                + (int)prestashop==0 ? 0 : 1;
+    }
+}
