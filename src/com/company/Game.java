47b47b21bd0b3475fed0c06f097afe41c9042453
hit02 <bagnucki02@gmail.com>
2020-06-08 14:56:32 +0200

    Add lists of Players, AvailableProjects and AvailableWorksers

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,4 +1,10 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private List<Player> Players = new ArrayList<>();
+    private List<Project> AvailableProjects = new ArrayList<>();
+    private List<Worker> AvailableWorkers = new ArrayList<>();
 }
