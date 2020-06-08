26012b9e55d19eac58d2522c3dc04288ae8ad6ad
hit02 <bagnucki02@gmail.com>
2020-06-08 13:55:14 +0200

    fix missing public property

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -3,7 +3,8 @@ package com.company;
 public class Human {
     public String firstName;
     public String lastName;
-    Human(String firstName, String lastName) {
+
+    public Human(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
