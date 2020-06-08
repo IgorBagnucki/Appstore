12658ce761f43a82022cf2b26f4696fcc2fde5f3
hit02 <bagnucki02@gmail.com>
2020-06-08 14:36:01 +0200

    implement getters and missing private properties

--- src/com/company/Programmer.java
+++ src/com/company/Programmer.java
@@ -1,9 +1,9 @@
 package com.company;
 
 public class Programmer extends Worker {
-    TechnologiesWrapper<Integer> knownTechnologies;
-    float workAccuracy;
-    int maximumDelay;
+    private TechnologiesWrapper<Integer> knownTechnologies;
+    private float workAccuracy;
+    private int maximumDelay;
     public Programmer(
             String firstName,
             String lastName,
@@ -13,5 +13,17 @@ public class Programmer extends Worker {
             TechnologiesWrapper<Integer> knownTechnologies) {
         super(firstName, lastName, expectedRemuneration);
         this.knownTechnologies = new TechnologiesWrapper<>(knownTechnologies);
+    }
+
+    public TechnologiesWrapper<Integer> getKnownTechnologies() {
+        return knownTechnologies;
+    }
+
+    public float getWorkAccuracy() {
+        return workAccuracy;
+    }
+
+    public int getMaximumDelay() {
+        return maximumDelay;
     }
 }
