518d03bd4e098817b66bd4926d309fd83a39f15b
hit02 <bagnucki02@gmail.com>
2020-06-08 13:53:04 +0200

    implement Human

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class Human {
+    public String firstName;
+    public String lastName;
+    Human(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
