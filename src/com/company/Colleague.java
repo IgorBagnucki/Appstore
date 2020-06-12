252d7dd79744d343e4e1872ebc32806777470788
hit02 <bagnucki02@gmail.com>
2020-06-12 21:35:31 +0200

    Implement Colleague and Player

--- src/com/company/Colleague.java
+++ src/com/company/Colleague.java
@@ -1,7 +1,42 @@
 package com.company;
 
-public class Colleague extends Human {
-    protected Colleague(String firstName, String lastName) {
-        super(firstName, lastName);
+public class Colleague extends Programmer {
+    public enum Type{
+        BEST,
+        MID,
+        COCKY
+    }
+    private Type type;
+    public Colleague(
+            SeniorityLevel seniorityLevel,
+            String firstName,
+            String lastName,
+            int expectedRemuneration,
+            double workAccuracy,
+            int maximumDelay,
+            TechnologiesWrapper<Integer> knownTechnologies,
+            Type type) {
+        super(
+                seniorityLevel,
+                firstName,
+                lastName,
+                expectedRemuneration,
+                workAccuracy,
+                maximumDelay,
+                knownTechnologies);
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        String value = firstName + " " + lastName + "\n\n";
+        if(type == Type.BEST) {
+            value += "He is the most expensive, but makes no mistakes and doesn't miss deadlines";
+        } else if(type == Type.MID) {
+            value += "He doesn't miss deadlines, but he has 10% chance to make errors";
+        } else {
+            value += "Cheapest, 20% chance he'll be late and 20% chance he'll make errors";
+        }
+        return value;
     }
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,26 @@
 package com.company;
 
-public class Player {
+public class Player extends Programmer {
+    public Player(
+        SeniorityLevel seniorityLevel,
+        String firstName,
+        String lastName,
+        int expectedRemuneration,
+        double workAccuracy,
+        int maximumDelay,
+        TechnologiesWrapper<Integer> knownTechnologies) {
+        super(
+            seniorityLevel,
+                firstName,
+                lastName,
+                expectedRemuneration,
+                workAccuracy,
+                maximumDelay,
+                knownTechnologies);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + "\n\nIt's you!";
+    }
 }
