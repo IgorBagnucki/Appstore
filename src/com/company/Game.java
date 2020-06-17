ae165a869bc8810e4f0863556688973eb1774761
hit02 <bagnucki02@gmail.com>
2020-06-17 20:34:56 +0200

    First draw of game loop

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,17 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    private List<Player> players = new ArrayList<>();
-    private List<Project> availableProjects = new ArrayList<>();
-    private List<Programmer> availableProgrammers = new ArrayList<>();
-    private List<Tester> availableTesters = new ArrayList<>();
-    private List<Seller> availableSellers = new ArrayList<>();
+    private final List<Player> players = new ArrayList<>();
+    private final List<Project> availableProjects = new ArrayList<>();
+    private final List<Programmer> availableProgrammers = new ArrayList<>();
+    private final List<Tester> availableTesters = new ArrayList<>();
+    private final List<Seller> availableSellers = new ArrayList<>();
 
-    private Menu mainGameMenu = new Menu();
-
+    private final Menu mainGameMenu = new Menu();
+    private final MenuOption employProgrammer = new MenuOption("Employ a programmer");
+    private final MenuOption employColleague = new MenuOption("Employ an colleague");
+    private final MenuOption employTester = new MenuOption("Employ a tester");
+    private final MenuOption employSeller = new MenuOption("Employ a seller");
+    private final MenuOption findProject = new MenuOption("Find new project");
+    private final MenuOption fireWorker = new MenuOption("Fire a worker");
 
     public Game() {
-        mainGameMenu.add(new MenuOption("asdf"));
+        mainGameMenu.add(employProgrammer);
+        mainGameMenu.add(employColleague);
+        mainGameMenu.add(employTester);
+        mainGameMenu.add(employSeller);
+        mainGameMenu.add(findProject);
+        mainGameMenu.add(fireWorker);
         for(int i = 0; i < 6; ++i) {
             availableProgrammers.add(ProgrammerGenerator.generate());
             availableTesters.add(TesterGenerator.generate());
@@ -22,7 +32,21 @@ public class Game {
         }
     }
 
-    public void turn() {
-
+    public boolean turn() {
+        MenuOption selectedOption = Interface.displayMenu(mainGameMenu);
+        if(selectedOption == employProgrammer) {
+            System.out.println(0);
+        } else if(selectedOption == employColleague) {
+            System.out.println(1);
+        } else if(selectedOption == employTester) {
+            System.out.println(2);
+        } else if(selectedOption == employSeller) {
+            System.out.println(3);
+        } else if(selectedOption == findProject) {
+            System.out.println(4);
+        } else if(selectedOption == fireWorker) {
+            System.out.println(5);
+        }
+        return true;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.add(new MenuOption("How many", "one", "two", "three"));
-        menu.add(new MenuOption("Music", "[ ]", "[x]"));
-        menu.add(new MenuOption("No choice"));
-        System.out.println(Interface.displayMenu(menu));
+        Game game = new Game();
+        while(game.turn());
     }
 }
