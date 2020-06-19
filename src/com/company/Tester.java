48e033b36af60e3718c29a853b2dec3d4b4fb55f
hit02 <bagnucki02@gmail.com>
2020-06-19 11:32:00 +0200

    Fix missing details() implementations

--- src/com/company/Tester.java
+++ src/com/company/Tester.java
@@ -4,4 +4,9 @@ public class Tester extends Worker {
     public Tester(String firstName, String lastName, int expectedRemuneration) {
         super(firstName, lastName, expectedRemuneration);
     }
+
+    @Override
+    public String details() {
+        return "";
+    }
 }
